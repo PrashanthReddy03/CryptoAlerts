41d3c8947386a81e9fce427256b0917930266e75
Jakku Prashanth Reddy <67828370+PrashanthReddy03@users.noreply.github.com>
2022-04-13 14:47:54 +0530

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,28 @@
+#TELEGRAM CONFIGURATION
+TELEGRAM_CHAT_ID = '@tech_alerts'
+TELEGRAM_BOT_API_KEY = 'bot1831759774:AAH9jXzAcX5oaNyf0pArzL6syysZQJuX8Zs'
+
+#CRYPTO_COMPARE CONFIGURATION
+BITCOIN_URL = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR'
+DOGE_URL = 'https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=INR'
+ETHEREUM_URL = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=INR'
+BAT_URL = 'https://min-api.cryptocompare.com/data/price?fsym=BAT&tsyms=INR'
+TNC_URL = 'https://min-api.cryptocompare.com/data/price?fsym=TNC&tsyms=INR'
+
+
+#THRESHOLDS
+
+BUY_BTC = 2252985
+SELL_BTC = 3056582
+
+BUY_ETH = 133302
+SELL_ETH = 192843
+
+BUY_DOGE = 12.94
+SELL_DOGE = 24.43
+
+BUY_BAT = 33.61
+SELL_BAT = 52.53
+
+BUY_TNC = 0.00001
+SELL_TNC = 3
